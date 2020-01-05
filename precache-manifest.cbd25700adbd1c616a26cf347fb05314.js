ba72f8bd7eceb683f8faaaefdc706ecb9b58b4ab
Javlonbek9393 <44919149+Javlonbek999@users.noreply.github.com>
2020-01-05 19:12:39 +0500

    Updates

---
+++ precache-manifest.cbd25700adbd1c616a26cf347fb05314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9691629ac94f18dbb791b18a304387",
+    "url": "/foodinfo/index.html"
+  },
+  {
+    "revision": "9b8a603bc38d7d71ffaa",
+    "url": "/foodinfo/static/css/main.b4637bd2.chunk.css"
+  },
+  {
+    "revision": "a0a803ccc9cae883f8c5",
+    "url": "/foodinfo/static/js/2.9e3545e0.chunk.js"
+  },
+  {
+    "revision": "9b8a603bc38d7d71ffaa",
+    "url": "/foodinfo/static/js/main.ba6af3c0.chunk.js"
+  },
+  {
+    "revision": "d253c17913d817663e58",
+    "url": "/foodinfo/static/js/runtime-main.4c0f7831.js"
+  }
+]);
